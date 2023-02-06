09954c02536103e8005cfb7aa8d6c49904bc15b8
NiccBotzz <124491010+NiccBotzz@users.noreply.github.com>
2023-02-06 12:58:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,12 +43,12 @@ global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
 global.Intervalmsg = 1800 //detik
 
 //========Url Template Buttons=========//
-global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
+global.dtu = '𝙶𝚒𝚝𝚑𝚞𝚋'
 global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 859-2108-9792'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
@@ -63,7 +63,7 @@ global.author = data.owner;
 
 //=========== Requirements ==========//  
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'ʙy ᴀɢᴜᴢ ꜰᴀᴍɪʟɪᴀ'
+        global.colong2 = 'ʙy Nicc'
 global.img = pickRandom(waifu)
 global.fla = pickRandom(flaaa)
 
